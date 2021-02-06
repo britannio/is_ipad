98c5fc36922b33212ac7c9990f67e3b42df9a4d6
Britannio <britanniojarrett@gmail.com>
2021-02-06 16:34:39 +0000

    migrate to null safety

--- lib/is_ipad.dart
+++ lib/is_ipad.dart
@@ -9,9 +9,11 @@ class IsIpad {
 
   static final IsIpad instance = IsIpad._();
   static const MethodChannel _channel = const MethodChannel('is_ipad');
-  bool _isIpad;
+  bool? _isIpad;
 
   Future<bool> isIpad() async =>
       _isIpad ??
-      !kIsWeb && Platform.isIOS && await _channel.invokeMethod<bool>('isIpad');
+      !kIsWeb &&
+          Platform.isIOS &&
+          await (_channel.invokeMethod<bool>('isIpad') as FutureOr<bool>);
 }
